3409e7b1527d097dc042c0fa9f7222e6ddc6a2e6
Anurag Singh <contact@anuragsingh.me>
2016-04-05 18:28:30 +0530

    V2.0

---
+++ public/junk/class-all-calls-grid-data-dummy.php
@@ -0,0 +1,19 @@
+<?php
+$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+//require_once( $parse_uri[0] . 'wp-load.php' );
+require_once( $parse_uri[0] . 'wp-config.php' );
+
+//var_dump($parse_uri);
+
+ DB_HOST; DB_NAME;
+
+
+
+
+
+
+
+
+?>
+
+
